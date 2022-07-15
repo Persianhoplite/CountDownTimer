7526c8d065aa87c64a0bf05e6573d06619adacef
Alireza <persianhoplite@gmail.com>
2022-07-15 17:42:23 +0430

    all four countdown complete

--- scripts/valentine.js
+++ scripts/valentine.js
@@ -3,15 +3,15 @@ const hoursEl =document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl =document.getElementById('seconds');
 const currentYear = new Date();
-const halloween = '14 February' + ' ' + currentYear.getFullYear();
+const valentine = '14 February ' + (currentYear.getFullYear() + 1);
 
 function countDown() {
-    const halloweenDate = new Date(halloween);
+    const valentineDate = new Date(valentine);
     const currentDate = new Date();
-    const days = parseInt(Math.abs(halloweenDate - currentDate) / (1000 * 60 * 60 * 24));
-    const hours = parseInt(Math.abs(halloweenDate - currentDate) / (1000 * 60 * 60) % 24);
-    const minutes = parseInt(Math.abs(halloweenDate.getTime() - currentDate.getTime()) / (1000 * 60) % 60);
-    const seconds = parseInt(Math.abs(halloweenDate.getTime() - currentDate.getTime()) / (1000) % 60); 
+    const days = parseInt(Math.abs(valentineDate - currentDate) / (1000 * 60 * 60 * 24));
+    const hours = parseInt(Math.abs(valentineDate - currentDate) / (1000 * 60 * 60) % 24);
+    const minutes = parseInt(Math.abs(valentineDate.getTime() - currentDate.getTime()) / (1000 * 60) % 60);
+    const seconds = parseInt(Math.abs(valentineDate.getTime() - currentDate.getTime()) / (1000) % 60); 
 
     daysEl.innerHTML = days;
     hoursEl.innerHTML = hours;
